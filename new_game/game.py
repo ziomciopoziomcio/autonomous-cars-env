1aa21d64e33e3a2437e956f6e0a8c2ecb4ec584e
Jędrzej Niedzielski <jedrzejnie@gmail.com>
2025-06-03 13:23:33 +0200

    added car to game

--- new_game/game.py
+++ new_game/game.py
@@ -1,6 +1,7 @@
 import pygame
 import json
 import os
+import math
 
 MAP_FILE = os.path.join("..", "map_generators", "map_data.json")
 
@@ -11,6 +12,37 @@ INNER_COLOR = (50, 50, 200)
 OUTER_COLOR = (200, 50, 50)
 TRACK_COLOR = (50, 200, 50)
 FINISH_COLOR = (255, 255, 0)
+
+class Car:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.angle = 0
+        self.speed = 0
+
+    def update(self):
+        # obsługa pygame klawiatury
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_LEFT]:
+            self.angle += 5
+        if keys[pygame.K_RIGHT]:
+            self.angle -= 5
+        if keys[pygame.K_UP]:
+            self.speed += 1
+        if keys[pygame.K_DOWN]:
+            self.speed -= 1
+        # Aktualizacja pozycji samochodu
+        self.x += self.speed * math.cos(math.radians(self.angle))
+        self.y -= self.speed * math.sin(math.radians(self.angle))
+
+
+
+    def draw(self, screen):
+        car_rect = pygame.Rect(self.x - 15, self.y - 10, 30, 20)
+        rotated_car = pygame.transform.rotate(pygame.Surface(car_rect.size), -self.angle)
+        rotated_car.fill((255, 0, 0))
+        rotated_rect = rotated_car.get_rect(center=car_rect.center)
+        screen.blit(rotated_car, rotated_rect.topleft)
 
 def load_map(file_path):
     with open(file_path, "r") as f:
@@ -42,6 +74,8 @@ def main():
     clock = pygame.time.Clock()
     data = load_map(MAP_FILE)
 
+    car = Car(WIDTH // 2, HEIGHT // 2)
+
     running = True
     while running:
         screen.fill(BG_COLOR)
@@ -50,6 +84,10 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+
+        car.update()
+        car.draw(screen)
+
 
         pygame.display.flip()
         clock.tick(60)
