b08e953b31ed768b8d8a102a4730a4ff63f0b91e
Jędrzej Niedzielski <jedrzejnie@gmail.com>
2025-08-19 18:28:09 +0200

    Added file with imports and global variables

---
+++ components/globals.py
@@ -0,0 +1,26 @@
+import pygame
+import json
+import os
+import math
+
+MAP_FILE = None
+FINISH_TEXTURE = None
+TRACK_IMAGE = None
+BACKGROUND_IMAGE = None
+
+# Constants
+WIDTH, HEIGHT = 1200, 800
+BG_COLOR = (30, 30, 30)
+INNER_COLOR = (200, 50, 50)  # (50, 50, 200)
+OUTER_COLOR = (200, 50, 50)
+TRACK_COLOR = (50, 200, 50)
+FINISH_COLOR = (255, 255, 0)
+CAR_SIZE_RATIO = 0.2  # Ratio of car size to track width
+ROW_OFFSET_FACTOR = 1.1
+OFFSET_DISTANCE_FACTOR = 2
+CAR_LENGTH_RATIO = 2
+CAR_SPACING_FACTOR = 1.5
+PERPENDICULAR_ANGLE_OFFSET = 90
+
+USED_CARS = 0
+COLORS = ["red-car.png", "white-car.png", "green-car.png", "grey-car.png", "purple-car.png"]
